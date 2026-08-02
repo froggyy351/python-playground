669908bb95fabb371cf605a07ed54b7aa5d1b8bb
sakoichi <kouichi19980817@gmail.com>
2026-08-03 08:07:06 +0900

    plant_nameからplant_priceの検索エンドポイント追加

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+plants = {"エバーフレッシュ": 20000, "アスパラガスナナス": 300, "ポトス": 500, "モンステラ": 8000}
 
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+
+@app.get("/plants/{plant_name}")
+def get_plant_price(plant_name: str):
+    plant_price = plants[plant_name]
+    return plant_price
+
