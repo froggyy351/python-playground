6128cad8dba7664eefb50c6e4689f1941689c52c
sakoichi <kouichi19980817@gmail.com>
2026-08-04 08:02:37 +0900

    Dependsを追加

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -7,21 +7,23 @@ plants = {"エバーフレッシュ": 20000, "アスパラガスナナス": 300,
 class PlantCreate(BaseModel):
     name: str
     price: int
-
+    
+def get_plants_db():
+    return plants
 
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
 
 @app.get("/plants/{plant_name}")
-def get_plant_price(plant_name: str):
-    if plant_name not in plants:
+def get_plant_price(plant_name: str, db: dict = Depends(get_plants_db)):
+    if plant_name not in db:
         raise HTTPException(status_code=404, detail=f"{plant_name}は見つかりません")
-    return plants[plant_name]
+    return db[plant_name]
 
 @app.get("/plants")
-def list_plants(min_price: int = 0):
-    result = {name: price for name, price in plants.items() if price >= min_price}
+def list_plants(min_price: int = 0, db: dict = Depends(get_plants_db)):
+    result = {name: price for name, price in db.items() if price >= min_price}
     return result
 
 @app.post("/plants")
