a26bf28ed690027dc1348cca427db58425290aec
sakoichi <kouichi19980817@gmail.com>
2026-08-05 08:04:41 +0900

    feat: update,delete

--- app.py
+++ app.py
@@ -30,3 +30,20 @@ def list_plants(min_price: int = 0, db: dict = Depends(get_plants_db)):
 def add_plant(plant: PlantCreate):
     plants[plant.name] = plant.price
     return {"message": f"{plant.name}を追加しました", "plants": plants}
+
+class PlantUpdate(BaseModel):
+    price: int
+
+@app.put("/plants/{plant_name}")
+def update_plant(plant_name: str,plant: PlantUpdate, db: dict = Depends(get_plants_db)):
+    if plant_name not in db:
+        raise HTTPException(status_code=404, detail=f"{plant_name}は見つかりません")
+    db[plant_name] = plant.price
+    return {"message": f"{plant_name}を更新しました", "plants": db}
+
+@app.delete("/plants/{plant_name}")
+def delete_plant(plant_name: str, db: dict = Depends(get_plants_db)):
+    if plant_name not in db:
+        raise HTTPException(status_code=404, detail=f"{plant_name}は見つかりません")
+    del db[plant_name]
+    return {"message": f"{plant_name}を削除しました", "plants": db}
